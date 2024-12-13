1fc3f8878be6a7c83a81237be636143bcb6b125a
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2024-12-13 16:38:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp53
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "1990";
+            string userInput;
+
+            int triesCount = 3;
+
+            for (int i = 0; i < triesCount; i++)
+            {
+                Console.Write("Ваши попытки - "+ (triesCount -i) + "\nВведите пароль: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine("Доступ разрешен!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Неверный пароль!");
+                }
+            }
+        }
+    }
+}
